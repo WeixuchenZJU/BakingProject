190c8e941cc5ea9013798a9ac61af7a914499475
WeixuchenZJU <3140100687@zju.edu.cn>
2018-03-14 15:50:39 +0800

    Sample on Triangle

--- PaperProject/OutputTriData.cpp
+++ PaperProject/OutputTriData.cpp
@@ -16,6 +16,13 @@ OutputTriData::OutputTriData(const char* root, Scene *scene)
 	datadir = root + datadir;
 	mkdir(datadir.c_str());
 	std::vector<Group> g = scene->GetGroup();
+	//Shading Info
+	int lightnum = scenemanager::lightnum;
+	glm::vec3 lightpos = scenemanager::lightpos[0];
+	glm::vec3 lightcolor = scenemanager::lightcolor[0];
+	glm::vec3 objcolor(1.0f, 1.0f, 1.0f);
+
+
 	for (int ig = 0; ig < g.size(); ig++) {
 		std::vector<Vertex> vertices = g[ig].GetVertices();
 			for (int i = 0; i < vertices.size()/3; i++) {
@@ -30,13 +37,19 @@ OutputTriData::OutputTriData(const char* root, Scene *scene)
 				fprintf(file, "v2 %f %f %f\n", vertices[i * 3 + 2].Position.x, vertices[i * 3 + 2].Position.y, vertices[i * 3 + 2].Position.z);
 				//输出采样点
 				fprintf(file, "#sp(s,t) r g b\n");
-				float s = 0.3, t = 0.3;
-				Vertex sp;
-				sp.Position = s * vertices[i * 3 + 0].Position + t * vertices[i * 3 + 1].Position +(1 - s - t)* vertices[i * 3 + 2].Position;
-				sp.Normal = s * vertices[i * 3 + 0].Normal + t * vertices[i * 3 + 1].Normal + (1 - s - t)* vertices[i * 3 + 2].Normal;
-				sp.TexCoords = s * vertices[i * 3 + 0].TexCoords + t * vertices[i * 3 + 1].TexCoords + (1 - s - t)* vertices[i * 3 + 2].TexCoords;
-
-
+				for (float s = 0; s <= 1; s += 0.01) {
+					for (float t = 0; t <= 1; t += 0.01) {
+						float a = 1 - sqrtf(t);
+						float b = (1 - s)*sqrtf(t);
+						float c = s * sqrtf(t);
+						Vertex sp;
+						sp.Position = a * vertices[i * 3 + 0].Position + b * vertices[i * 3 + 1].Position + c* vertices[i * 3 + 2].Position;
+						sp.Normal = a * vertices[i * 3 + 0].Normal + b * vertices[i * 3 + 1].Normal +c* vertices[i * 3 + 2].Normal;
+						sp.TexCoords = a * vertices[i * 3 + 0].TexCoords + b * vertices[i * 3 + 1].TexCoords + c* vertices[i * 3 + 2].TexCoords;
+						glm::vec3 color = glm::max(0.0f,glm::dot(glm::normalize(sp.Normal), glm::normalize(lightpos - sp.Position)))*lightcolor*objcolor;
+						fprintf(file, "%f %f %f %f %f\n", a, b, color.r, color.g, color.b);
+					}
+				}
 				fclose(file);
 			}
 	}
