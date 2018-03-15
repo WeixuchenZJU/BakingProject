22b01b6a3107a81df9f1bff6fce22566d50b5c11
WeixuchenZJU <3140100687@zju.edu.cn>
2018-03-15 17:37:33 +0800

    20180315

--- PaperProject/MatLab/Interpolation.m
+++ PaperProject/MatLab/Interpolation.m
@@ -1,42 +1,25 @@
-% % x=0:0.01:pi;
-% % y=sin(x);
-% % plot(x,y);
-% t=0:0.01:1;
-% % x = 1-t; 
-% % y = t; 
-% B1=(1-t).^3;
-% B2=3*t.*((1-t).^2);
-% B3=3*(t.^2).*(1-t);
-% B4=t.^3;
-% 
-% plot(t,B1,t,B2,t,B3,t,B4);
-% xlabel('t')
-% text(0.09,0.8,'B_1(t)')
-% text(0.84,0.8,'B_4(t)')
-% text(0.29,0.46,'B_2(t)')
-% text(0.64,0.46,'B_3(t)')
-% subplot(1,3,1);
-% plot(t,x,t,y);
-% subplot(1,3,2);
-% plot(t,x,t,y);
-% subplot(1,3,3);
-% plot(t,x,t,y);
-%计算采样点处的颜色
-for i=0:11
-tri0=load(strcat(strcat('F:/FinalProject/PaperProject/BakingData/cube/t',num2str(i,'%d')),'.txt'));
-v0= tri0(1,3:5)';
-v1=tri0(2,3:5)';
-v2=tri0(3,3:5)';
+for i=1:12
+tri0=load(strcat(strcat('..\\BakingData\\cube\\t',num2str(i-1,'%d')),'.txt'));
+vv0= tri0(1,3:5)';
+vv1=tri0(2,3:5)';
+vv2=tri0(3,3:5)';
+%将三维空间上的点投影到xoy平面上
+v0=[0,0,0]';
+v1=[norm(vv1-vv0),0,0]';
+e12=norm(vv2-vv1);
+e02=norm(vv2-vv0);
+a=v1(1);
+c=(e02*e02-e12*e12+a*a)/(2*a);
+b=sqrt(e02*e02-c*c);
+v2=[c,b,0]';
+subplot(3,4,i);
 DrawTriangle(v0,v1,v2);
-for j=4:1020
-s=tri0(j*10,1);
-t=tri0(j*10,2);
-sp=s*v0+t*v1+(1-s-t)*v2;
-plot3(sp(1),sp(2),sp(3),'.','Color',tri0(j*10,3:5));
-hold on;
-end
-
-hold on;
+    for j=4:10204
+        s=tri0(j,1);
+        t=tri0(j,2);
+        sp=s*v0+t*v1+(1-s-t)*v2;
+        plot3(sp(1),sp(2),tri0(j,3),'.','Color',tri0(j,3:5));
+    end
 end
 
 % [sp,v0,v1,v2]=UniformSampleTri(v0,v1,v2,2000);
